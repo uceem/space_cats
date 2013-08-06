9af0509c1e3381a6889876652864660dee6e30aa
johnotander <johnotander@gmail.com>
2013-08-06 16:20:46 -0600

    Begin method with a hardcoded img url.

--- lib/space_cats.rb
+++ lib/space_cats.rb
@@ -1,5 +1,11 @@
 require "space_cats/version"
 
 module SpaceCats
-  # Your code goes here...
+  module InstanceMethods
+    def space_cat
+      "<img src='http://24.media.tumblr.com/ad579a792a0bd66f8e52ecad2e8484b0/tumblr_mmpha2CBUT1r4xjo2o1_250.gif' href='http://24.media.tumblr.com/ad579a792a0bd66f8e52ecad2e8484b0/tumblr_mmpha2CBUT1r4xjo2o1_250.gif'></img>"
+    end
+  end
 end
+
+ActionView::Base.send :include, SpaceCats if defined?(ActionView::Base)
