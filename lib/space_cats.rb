90cea2f2f16e4afd37e99f3bed867f56c2a0ec60
johnotander <johnotander@gmail.com>
2013-08-06 17:59:23 -0600

    Add all omgcatsinspace files.

--- lib/space_cats.rb
+++ lib/space_cats.rb
@@ -1,11 +1,35 @@
 require "space_cats/version"
 
 module SpaceCats
-  module InstanceMethods
-    def space_cat
-      "<img src='http://24.media.tumblr.com/ad579a792a0bd66f8e52ecad2e8484b0/tumblr_mmpha2CBUT1r4xjo2o1_250.gif' href='http://24.media.tumblr.com/ad579a792a0bd66f8e52ecad2e8484b0/tumblr_mmpha2CBUT1r4xjo2o1_250.gif'></img>"
+  def space_cat
+    "<a href='file.read.split.grep(/http[s]?:\/\/\w/).first'><img src='#{ get_cat_url }'></img></a>".html_safe
+  end
+
+  def get_cat_url
+    begin
+      parse_url_from_string(File.open(get_random_space_cat_file))
+    rescue
+      return pizza_cat_url
     end
   end
+
+  private
+
+    def get_random_space_cat_file
+      possible_space_cat_files.shuffle.first
+    end
+
+    def possible_space_cat_files
+      Dir["#{ File.join(File.dirname(File.dirname(__FILE__)), 'omgcatsinspace') }/*"]
+    end
+
+    def parse_url_from_string(file)
+      file.read.split.grep(/http[s]?:\/\/\w/).first
+    end
+
+    def pizza_cat_url
+      'http://24.media.tumblr.com/42181e20797b546c2f842ce8f6bf9f55/tumblr_mmel6b5X5K1r4xjo2o1_250.gif'
+    end
 end
 
-ActionView::Base.send :include, SpaceCats if defined?(ActionView::Base)
+ActionView::Base.send(:include, SpaceCats) if defined?(ActionView::Base)
